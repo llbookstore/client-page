4c62af7be4b1aeb840e293473e3469d441f39db2
LuongLegend <luongprolv99@gmail.com>
2021-04-01 08:53:07 +0700

    add redux-thunk

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
 import { persistStore } from 'redux-persist';
 import reducers from './reducers/index';
-
-export const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const middleware = [thunk];
+export const store = createStore(reducers, composeWithDevTools(applyMiddleware(...middleware)));
 
 export const persistor = persistStore(store)
