1fe503b626308c0c0498ad80094ad874065db264
LuongLegend <luongprolv99@gmail.com>
2021-04-22 11:26:36 +0700

    fix getBillDetails can get another user`s bill details

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -17,21 +17,21 @@ const getPaymentType = (type) => {
     const findType = paymentTypes.find(item => item.key === type);
     return findType ? findType.title : '';
 }
-const OrderDetails = (props) => {
+const OrderDetails = ({ user }) => {
     const { id } = useParams();
     const [orderData, setOrderData] = useState();
     const [widthScreen, setWidthScreen] = useState(window.innerWidth);
     useEffect(() => {
         const getUserOrder = async () => {
-            const getOrder = await callApi(`bill/${id}`, 'GET');
+            const getOrder = await callApi(`bill/${id}`, 'GET', { user_id: user.account_id });
             if (getOrder && getOrder.status === 1) {
                 setOrderData(getOrder.data);
             }
         }
         getUserOrder();
-    }, [id])
+    }, [id, user.account_id])
     useEffect(() => {
-        window.addEventListener('resize', () =>{
+        window.addEventListener('resize', () => {
             setWidthScreen(window.innerWidth);
         })
     }, [])
@@ -90,54 +90,61 @@ const OrderDetails = (props) => {
     return (
         <div>
             {
-                !orderData ? <UnFindPage /> : 
-                <>
-                    <Row justify='space-between'>
-                        <span className='order-detail__title'>
-                            Chi tiết đơn hàng: #{id} - {getOrderStatus(orderData.status)}
-                        </span>
-                        <span className='order-detail__date-buy'>
-                            Ngày đặt hàng: {timestampToDate(orderData.created_at, 'DD/MM/YYYY, LT')}
-                        </span>
-                    </Row>
-                    <hr />
-                    <Row gutter={[16, 24]} orientation="left" >
-                        <Col flex='3' >
-                            <Card title='ĐỊA CHỈ NGƯỜI NHẬN' style={{ fontSize: '1.4em', minHeight: '250px' }}>
-                                <Title level={4}>{orderData.user_name}</Title>
-                                <strong>Địa chỉ:</strong> {orderData.address} <br />
-                                <strong>Điện thoại:</strong> {orderData.phone}
-                            </Card>
-                        </Col>
-                        <Col flex='2' >
-                            <Card title='HÌNH THỨC THANH TOÁN' style={{ fontSize: '1.4em', minHeight: '250px' }}>
-                                {getPaymentType(orderData.payment_method)}
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Table
-                        columns={columns}
-                        dataSource={orderData.bill_details}
-                        rowKey={(record) => record.book_id}
-                        style={{ marginTop: '2em' }}
-                        pagination={false}
-                    />
-                    <Card >
+                !orderData ? <UnFindPage /> :
+                    <>
                         <Row justify='space-between'>
-                        <Title level={4}>
-                                Tổng cộng
-                        </Title>
-                            <NumberFormat className='price-total' value={orderData.total_price} displayType={'text'} thousandSeparator={true} />
+                            <span className='order-detail__title'>
+                                Chi tiết đơn hàng: #{id} - {getOrderStatus(orderData.status)}
+                            </span>
+                            <span className='order-detail__date-buy'>
+                                Ngày đặt hàng: {timestampToDate(orderData.created_at, 'DD/MM/YYYY, LT')}
+                            </span>
                         </Row>
-                    </Card>
-                </>
+                        <hr />
+                        <Row gutter={[16, 24]} orientation="left" >
+                            <Col flex='3' >
+                                <Card title='ĐỊA CHỈ NGƯỜI NHẬN' style={{ fontSize: '1.4em', minHeight: '250px' }}>
+                                    <Title level={4}>{orderData.user_name}</Title>
+                                    <strong>Địa chỉ:</strong> {orderData.address} <br />
+                                    <strong>Điện thoại:</strong> {orderData.phone}
+                                </Card>
+                            </Col>
+                            <Col flex='2' >
+                                <Card title='HÌNH THỨC THANH TOÁN' style={{ fontSize: '1.4em', minHeight: '250px' }}>
+                                    {getPaymentType(orderData.payment_method)}
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Table
+                            columns={columns}
+                            dataSource={orderData.bill_details}
+                            rowKey={(record) => record.book_id}
+                            style={{ marginTop: '2em' }}
+                            pagination={false}
+                        />
+                        {
+                            orderData.user_note &&
+                            <Card style={{ fontSize: '1.4em' }}>
+                                <strong>Ghi chú:</strong>
+                                {orderData.user_note}
+                            </Card>
+                        }
+                        <Card >
+                            <Row justify='space-between'>
+                                <Title level={4}>
+                                    Tổng cộng
+                        </Title>
+                                <NumberFormat className='price-total' value={orderData.total_price} displayType={'text'} thousandSeparator={true} />
+                            </Row>
+                        </Card>
+                    </>
             }
         </div>
     )
 }
 
-const mapStateToProps = ({ products }) => {
-    return { products }
+const mapStateToProps = ({ user }) => {
+    return { user }
 }
 
 export default connect(mapStateToProps)(OrderDetails);
