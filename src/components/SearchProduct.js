69d5780c966f12505505bc6e42f0a953e1042fb1
LuongLegend <luongprolv99@gmail.com>
2021-05-27 03:48:56 +0700

    add pagnation to search product

--- src/components/SearchProduct.js
+++ src/components/SearchProduct.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { connect } from 'react-redux'
 import ProductList from './ProductList'
-
+import { Pagination } from 'antd'
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
@@ -11,21 +11,34 @@ const SearchProduct = (props) => {
     const { products } = props;
     const query = useQuery();
     const q = query.get('q');
-    const listProduct = products
-        .filter(item => item.name.includes(q)
+    const listProduct = (page = 1, pageSize = 8) => {
+        return products
+            .filter(item => item.name.includes(q)
                 || (item.author && item.author.name.includes(q))
                 || item.description.includes(q)
-        );
+            )
+            .slice(pageSize * (page - 1), pageSize * page)
+    }
+    const [data, setData] = useState(listProduct(1, 8));
+    useEffect(() => {
+        setData(listProduct(1, 8))
+        // eslint-disable-next-line
+    }, [q])
     console.log('list', listProduct)
     return (
-        <>
-            {
-                <ProductList
-                    products={listProduct}
-                    title={`Kết quả tìm kiếm: ${q}`}
-                />
-            }
-        </>
+        <div style={{ marginTop: '138px' }}>
+
+            <ProductList
+                products={data}
+                title={`Kết quả tìm kiếm: ${q}`}
+            />
+            <Pagination
+                total={listProduct(1, 100000).length}
+                // pageSize={8}
+                showSizeChanger={false}
+                onChange={page => setData(listProduct(page, 8))}
+            />
+        </div>
     )
 }
 const mapStateToProps = (state) => {
